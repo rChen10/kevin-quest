24824684340143661923f08da6371b07d35c80d0
Dimitri <dimitrisantiago626@gmail.com>
2020-02-15 20:00:40 -0500

    Chest opens, displays message. Message disappears on move. Camera zoom. Fixed Tiles.

--- player/Sprite.gd
+++ player/Sprite.gd
@@ -1,6 +1,14 @@
 extends StaticBody2D
 
-const FACE = Vector2(0, -1)
+var opened = false
+
+func _process(delta):
+	if get_parent().get_node("Player").velocity.x != 0 or get_parent().get_node("Player").velocity.y != 0:
+		$ChestMessage1.visible = false
 
 func chest_open():
-	$AnimatedSprite.play("open")
+	if opened == false:
+		$AnimatedSprite.play("open")
+		$ChestMessage1.visible = true
+		opened = true
+		
