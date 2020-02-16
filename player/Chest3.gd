48ba7fe033bffe9438a272b7700640abc7926528
Dimitri <dimitrisantiago626@gmail.com>
2020-02-16 01:55:05 -0500

    First fully playable version!

--- player/Chest3.gd
+++ player/Chest3.gd
@@ -4,12 +4,11 @@ var opened = false
 
 func _process(delta):
 	if get_parent().get_node("Player").velocity.x != 0 or get_parent().get_node("Player").velocity.y != 0:
-		$ChestMessage1.visible = false
+		$ChestMessage3.visible = false
 
 func chest_open():
 	if opened == false:
 		$AnimatedSprite.play("open")
-		$ChestMessage1.visible = true
 		get_parent().get_node("Player").stamina += 45
+		$ChestMessage3.visible = true
 		opened = true
-		
--- player/Chest4.gd
+++ player/Chest4.gd
@@ -4,12 +4,12 @@ var opened = false
 
 func _process(delta):
 	if get_parent().get_node("Player").velocity.x != 0 or get_parent().get_node("Player").velocity.y != 0:
-		$ChestMessage1.visible = false
+		$ChestMessage4.visible = false
 
 func chest_open():
 	if opened == false:
 		$AnimatedSprite.play("open")
-		$ChestMessage1.visible = true
-		get_parent().get_node("Player").stamina += 45
+		$ChestMessage4.visible = true
+		get_parent().get_node("Player").seasharp = true
+		get_parent().get_node("Player").stamina += 75
 		opened = true
-		
